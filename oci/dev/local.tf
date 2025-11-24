be4df0825825a8f4e28b9b4b0c77aec52f2d1386
Frank Pejril <frank.pejril@gmail.com>
2025-11-23 19:33:19 -0600

    Update ssh public key

--- oci/dev/local.tf
+++ oci/dev/local.tf
@@ -4,7 +4,7 @@ locals {
   public_sn_cidr  = "10.42.0.0/24"
   private_sn_cidr = "10.42.1.0/24"
   tenancy_ocid    = "ocid1.tenancy.oc1..aaaaaaaar2baaildkhge35qjg5vavyqc3oebn3miul5jfu6uyt5lqpjifoga"
-  ssh_public_key  = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJUDf4vz+6QMNIVliVFE/V3qfssk5b14HDhA3oYgJeN+"
+  ssh_public_key  = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJUDf4vz+6QMNIVliVFE/V3qfssk5b14HDhA3oYgJeN+ fpejril@Franks-MacBook-Pro.local"
 
   ad_names        = data.oci_identity_availability_domains.ads.availability_domains[*].name
   instance_prefix = "instance-${local.environment}"
